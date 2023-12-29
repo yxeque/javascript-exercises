6360c68b6e4d448a503eee4ec911ab864943badf
yxeque <yxequel@gmail.com>
2023-12-29 18:15:18 +0800

    Add solution repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = '';
+    if (num < 0) {
+        return 'ERROR';
+    } else {
+        for (let i = 1; i <= num; i++) {
+            repeatedString += string;
+        }
+        return repeatedString;
+    }
 };
+
+// repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
